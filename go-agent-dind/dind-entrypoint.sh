eda6a18b5489ff14a4f4b6b393952ddf23b727e5
YongliuLi <ylli@thoughtworks.com>
2016-09-29 18:45:51 +0800

    [#0] add aliyun registry-mirror

--- go-agent-dind/dind-entrypoint.sh
+++ go-agent-dind/dind-entrypoint.sh
@@ -1,2 +1,11 @@
-/usr/local/bin/dockerd-entrypoint.sh &
+#!/bin/sh
+set -e
+if [ "$#" -eq 0 -o "${1:0:1}" = '-' ]; then
+        set -- docker daemon \
+                --host=unix:///var/run/docker.sock \
+                --host=tcp://0.0.0.0:2375 \
+                --registry-mirror=https://8xwd88nd.mirror.aliyuncs.com
+                --storage-driver=vfs \
+                "$@"
+fi
 /run/go-agent/agent.sh
